47babecbb3d93a0a497099a228a3daf5b6e35b98
jbooher <john.c.booher@gmail.com>
2016-04-06 13:54:17 -0500

    Completed explorer mode and added validation of characters.

--- app/modules/teams/controller.js
+++ app/modules/teams/controller.js
@@ -1,46 +1,58 @@
-/*
-  Teams Controller Class
-  ----------------------
-
-  1. Create placeholders for name and characters.
-     name should be an empty string. It represents the
-     data in the input field in your view. characters
-     should be an empty array. It's going to hold your
-     team. You're going to loop over them to display
-     your team.
-
-  2. Fill out addCharacter. It should call the Marvel API
-    at: http://gateway.marvel.com:80/v1/public/characters?name=<name here>&apikey=<api key here>
-
-    Name should be from your input field (this.name)
-
-  3. When you get your response from the Marvel API, use
-    it to create a new Character. I've provided the class
-    already. It wants a name, description and image. Remember,
-    we combine the image from two fields in the response.
-
-  4. Add the character you just made to the characters array.
-
-  5. Fill out the deleteCharacter function. It's going to
-    receive a character. You want to find that character
-    in your characters array and remove it. Look up splice
-    on MDN if you don't remember how to do this.
-*/
-
 import Character from './Character';
 
 class TeamsController {
 
 	constructor($http) {
     this._$http = $http;
+		this.name = "";
+		this.characters = [];
 	}
 
   addCharacter() {
+		this._$http
+			.get(`http://gateway.marvel.com:80/v1/public/characters?name=${this.name}&apikey=6e7bd33438a14b84d91097cd3cfc46b5`)
+			.then((response) => {
+				console.log(response);
+				let char = new Character(
+					response.data.data.results[0].name,
+					response.data.data.results[0].description,
+					`${response.data.data.results[0].thumbnail.path}.${response.data.data.results[0].thumbnail.extension}`);
+				console.log(char);
+
+				if(this.validateAdd(char)) {
+					this.characters.push(char);
+				}
+
+				console.log(this.characters);
+				this.name = "";
+			}).catch((error) => {
+				console.log(`Error: ${error}`);
+				alert(`Could not find that hero: ${this.name}`)
+			})
+
   }
 
   deleteCharacter(character) {
+		this.characters.splice(this.characters.indexOf(character), 1);
   }
+
+	validateAdd(char){
+		console.log("validating");
+		let valid = true
+
+		this.characters.forEach((character) => {
+			console.log(character.name);
+			console.log(char.name);
+
+			if (character.name === char.name) {
+				alert("You have already added that character");
+				valid = false;
+			}
+		});
+
+		return valid;
+	}
 
 }
 
-export default TeamsController
+export default TeamsController;
